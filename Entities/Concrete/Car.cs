2d0fd885499fb37e6b8c51df6e3ef8447b463277
barış Fırat arslan <barisfiratarslan@hotmail.com>
2021-02-11 17:47:53 +0300

    Add project files.

---
+++ Entities/Concrete/Car.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Car:ICar
+    {
+        public int ID { get; set; }
+        public int BrandID { get; set; }
+
+    }
+}
