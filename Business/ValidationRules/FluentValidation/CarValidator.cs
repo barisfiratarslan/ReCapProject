fc125f3898d61ad9c95deb9184b3cba641c1e691
barış Fırat arslan <barisfiratarslan@hotmail.com>
2021-02-21 02:23:57 +0300

    Updated 21-02-2021 (FluentValidation desteği eklendi)

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.Name).MinimumLength(2);
+            RuleFor(c => c.DailyPrice).GreaterThan(0);
+            RuleFor(c => c.Name).NotEmpty();
+            RuleFor(c => c.BrandID).NotEmpty();
+            RuleFor(c => c.ColorID).NotEmpty();
+            RuleFor(c => c.ModelYear).NotEmpty();
+        }
+    }
+}
---
+++ Business/ValidationRules/FluentValidation/UserValidation.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class UserValidation:AbstractValidator<User>
+    {
+        public UserValidation()
+        {
+            RuleFor(u => u.FirstName).NotEmpty();
+            RuleFor(u => u.LastName).NotEmpty();
+            RuleFor(u => u.Email).NotEmpty();
+            RuleFor(u => u.Password).NotEmpty();
+        }
+    }
+}
